d6f54490b05f88b73c0ddc234539064ddeab68d2
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 18:55:36 +0200

    ISquareViewModel interface

--- SquareProject/Client/Program.cs
+++ SquareProject/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SquareProject.Client;
+using SquareProject.ViewModels;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
