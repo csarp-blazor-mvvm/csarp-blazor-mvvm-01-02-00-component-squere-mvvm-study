ec79ec6ca5687f90e7276bf07347f79f86c2fbb4
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 18:58:53 +0200

    A SquareViewModel osztály az ISquareViewModel interfactól is öröklődik

--- SquareProject/Client/ViewModels/SquareViewModel.cs
+++ SquareProject/Client/ViewModels/SquareViewModel.cs
@@ -1,9 +1,10 @@
-﻿using SquareProject.Models;
+﻿using SquareProject.Client.ViewModels;
+using SquareProject.Models;
 using ViewModels.BaseClass;
 
 namespace SquareProject.ViewModels
 {
-    public class SquareViewModel : ViewModelBase
+    public class SquareViewModel : ViewModelBase, ISquareViewModel
     {
         private Square _square;
 
